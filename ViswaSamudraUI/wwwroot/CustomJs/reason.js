3721699b1dca4dce64450994856dd47c94890cd2
NagarjunSaragadam <thenagarjun558@gmail.com>
2022-10-29 02:56:18 +0530

    Growl notification made universal

--- ViswaSamudraUI/wwwroot/CustomJs/reason.js
+++ ViswaSamudraUI/wwwroot/CustomJs/reason.js
@@ -1,46 +1,4 @@
-﻿$(document).ready(function () {
-    function notify(from, align, icon, type, animIn, animOut, msg) {
-        $.growl({
-            icon: icon,
-            title: ' Reason - ',
-            message: $('#ReasonCode').val() + ' - ' + $('#ReasonName').val() + ':' + msg,
-            url: ''
-        }, {
-            element: 'body',
-            type: type,
-            allow_dismiss: true,
-            placement: {
-                from: from,
-                align: align
-            },
-            offset: {
-                x: 30,
-                y: 30
-            },
-            spacing: 10,
-            z_index: 999999,
-            delay: 2500,
-            timer: 1000,
-            url_target: '_blank',
-            mouse_over: false,
-            animate: {
-                enter: animIn,
-                exit: animOut
-            },
-            icon_type: 'class',
-            template: '<div data-growl="container" class="alert" role="alert">' +
-                '<button type="button" class="close" data-growl="dismiss">' +
-                '<span aria-hidden="true">&times;</span>' +
-                '<span class="sr-only">Close</span>' +
-                '</button>' +
-                '<span data-growl="icon"></span>' +
-                '<span data-growl="title"></span>' +
-                '<span data-growl="message"></span>' +
-                '<a href="#!" data-growl="url"></a>' +
-                '</div>'
-        });
-    };
-    
+﻿$(document).ready(function () {    
     // [ notification-button ]
     $('.notifications.btn').on('click', function (e) {
         e.preventDefault();
@@ -108,12 +66,12 @@
                 success: function (data) {
                     nType = 'success';
                     message = data;
-                    notify(nFrom, nAlign, nIcons, nType, nAnimIn, nAnimOut, message)
+                    notify(nFrom, nAlign, nIcons, nType, nAnimIn, nAnimOut, $('#ReasonCode').val() + ' - ' + $('#ReasonName').val() + ' : '+ message, " Reason ")
                 },
                 error: function (xhr, ajaxOptions, thrownError) {
                     nType = 'danger';
                     message = 'Error In Updation';
-                    notify(nFrom, nAlign, nIcons, nType, nAnimIn, nAnimOut, message)
+                    notify(nFrom, nAlign, nIcons, nType, nAnimIn, nAnimOut, message," Reason ")
                 },
 
             });
